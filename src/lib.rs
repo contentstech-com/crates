8b4423c2cb08d39acd67919a73f9bc6e0670eee6
Hyeon Kim <simnalamburt@gmail.com>
2025-01-16 19:06:54 +0900

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@
 //!
 //! # Primary Focuses
 //!
-//! - **Performant**: The parser utilizes SIMD operations, therefore is very performant.
+//! - **Vectorized**: The parser utilizes SIMD operations, therefore is very performant.
 //! - **Minimal hidden costs**: Every API doesn't bring any invisible overheads, and each operation only does what it needs to do.
-//! - **Near-zero-allocation**: The parser doesn't allocate any memory during parsing and only performs allocation when dequoting each cell.
+//! - **Zero copy, zero allocation by default**: The parser doesn't allocate any memory during parsing and only performs allocation when dequoting each cell.
+//! - **Lazy Decoding**: Input is not copied or unquoted until requested. This is useful when you only need to access a few cells in a large CSV file.
 //! - **`#![no_std]` by default**: The crate is `#![no_std]` by default, and it can be used in `#![no_std]` environments without any additional configuration.
 //!
 //! # Supported Features
